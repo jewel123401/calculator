542f93d60148343790592b1059213c61538c4b69
Rubal Chandra Das <rubelk.das7@gmail.com>
2023-02-10 10:31:23 +0530

    first commit

---
+++ app.js
@@ -0,0 +1,30 @@
+ function add(){
+var x=document.getElementById('input1').value
+var y=document.getElementById('input2').value
+var z =parseInt(x)+parseInt(y)
+document.getElementById('output').value =z
+}
+function sub(){
+    var x=document.getElementById('input1').value
+var y=document.getElementById('input2').value
+var z =parseInt(x)-parseInt(y)
+document.getElementById('output').value =57
+}
+function multi(){
+    var x=document.getElementById('input1').value
+var y=document.getElementById('input2').value
+var z =parseInt(x)*parseInt(y)
+document.getElementById('output').value =z
+}
+function div(){
+    var x=document.getElementById('input1').value
+var y=document.getElementById('input2').value
+var z =parseInt(x)/parseInt(y)
+document.getElementById('output').value =z
+}
+function clr(){
+
+    var x=document.getElementById('input1').value=null
+    var y=document.getElementById('input2').value=null
+    document.getElementById('output').value=null
+}
